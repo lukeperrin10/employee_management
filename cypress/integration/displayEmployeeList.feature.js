b2f7a23f6e840dc59038ae5c995fa725850a04f4
DavDan1 <david-danielyan@hotmail.com>
2021-04-12 14:08:36 +0200

    added testing for list of employees

--- cypress/integration/displayEmployeeList.feature.js
+++ cypress/integration/displayEmployeeList.feature.js
@@ -1,9 +1,22 @@
 describe("Display list of employees", () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit("/");
   });
 
   it("displays a header", () => {
     cy.get("#header").should("contain", "Employee List");
+  });
+  it("display a list with 6 items", () => {
+    cy.get("#employee-list").withi(() => {
+      cy.get(".employee-item").should("have.length", 6);
+    });
+  });
+  it("the list items display the expected content", () => {
+    cy.get("#employee-item").within(() => {
+      cy.get(".employee-item")
+        .first()
+        .find(".name")
+        .should("contain", "George Bluth");
+    });
   });
 });
