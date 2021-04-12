3fe3199d9a50d1b4558c1463b0a40305f83ea0fa
Luke Perrin <lukeperrin27@gmail.com>
2021-04-12 13:19:35 +0200

    added first testt

---
+++ cypress/integration/displayEmployeeList.feature.js
@@ -0,0 +1,9 @@
+describe("Display list of employees", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+});
+
+it("displays a header", () => {
+  cy.get("#header").should("contain", "Employee List");
+});
