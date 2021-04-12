9f8c7c276349a765c67a9c0e22ab0f3c57880e2b
Luke Perrin <lukeperrin27@gmail.com>
2021-04-12 14:32:49 +0200

    added file for employee list and created implementation, all tests run green

--- cypress/integration/displayEmployeeList.feature.js
+++ cypress/integration/displayEmployeeList.feature.js
@@ -7,12 +7,12 @@ describe("Display list of employees", () => {
     cy.get("#header").should("contain", "Employee List");
   });
   it("display a list with 6 items", () => {
-    cy.get("#employee-list").withi(() => {
+    cy.get("#employee-list").within(() => {
       cy.get(".employee-item").should("have.length", 6);
     });
   });
   it("the list items display the expected content", () => {
-    cy.get("#employee-item").within(() => {
+    cy.get("#employee-list").within(() => {
       cy.get(".employee-item")
         .first()
         .find(".name")
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import EmployeeList from "./EmployeeList"
 
  class App extends Component {
   render() {
     return (
       <div>
         <h1 id="header">Employee List</h1>
+        <EmployeeList />
       </div>
     )
   }
